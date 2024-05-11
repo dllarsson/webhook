52c90463faed282d4ee1c372d24d0286747469cb
David Larsson <david.l.larsson.90@gmail.com>
2024-05-11 11:41:38 +0200

    config file added

---
+++ webhook.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const https = require('https')
+const config = require('config')
+const port = config.get('server.port')
+const cts = {
+    cert: fs.readFileSync(config.get('cert.fullchainPath')),
+    key: fs.readFileSync(config.get('cert.privkeyPath'))
+}
+
+app.post('/webhook', (req, res) => {
+    console.log('triggered branch: ' + req.body.ref)
+    console.log('...')
+    console.log(req.body)
+    res.send('Ok')
+})
+
+
+https.createServer(cts, app).listen(port);
